68e2c2bd9cdcb342c1426c4fca53e65df7fb258e
hippoantuttle <bkk0308@naver.com>
2022-07-31 16:05:46 +0900

    Why-use-useEffect?

--- src/App.js
+++ src/App.js
@@ -4,14 +4,37 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setCounter((prev) => prev + 1);
-  console.log("i run all the time");
+  const onChange = (event) => setKeyword(event.target.value);
 
   useEffect(() => {
-    console.log("call the Api");
+    console.log("I run only once.");
   }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("You are searching", keyword);
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  // keyword가 변화할때만 console.log("SEARCH FOR", keyword);를 실행하겠다는 의미
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when keyword & counter changes.");
+  }, [keyword, counter]);
+
   return (
     <div>
+      <input
+        value={keyword}
+        type="text"
+        placeholder="Search here..."
+        onChange={onChange}
+      />
       <h1 className={styles.title}>{counter}</h1>
       <Button text={"Continue"} />
       <button onClick={onClick}>Click me</button>
