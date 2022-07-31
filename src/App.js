291b0241f654aca57510683accf95c9ec26f3155
hippoantuttle <bkk0308@naver.com>
2022-07-31 16:47:39 +0900

    Cleanupfunc-for-understand

--- src/App.js
+++ src/App.js
@@ -1,43 +1,24 @@
-import Button from "./Button";
-import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
+function Hello() {
+  function byFn() {
+    console.log("destroyed");
+  }
+  function hiFn() {
+    console.log("created");
+    return byFn;
+  }
+  useEffect(hiFn, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
-  useEffect(() => {
-    console.log("I run only once.");
-  }, []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("You are searching", keyword);
-    }
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  // keyword가 변화할때만 console.log("SEARCH FOR", keyword);를 실행하겠다는 의미
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when keyword & counter changes.");
-  }, [keyword, counter]);
-
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        type="text"
-        placeholder="Search here..."
-        onChange={onChange}
-      />
-      <h1 className={styles.title}>{counter}</h1>
-      <Button text={"Continue"} />
-      <button onClick={onClick}>Click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
